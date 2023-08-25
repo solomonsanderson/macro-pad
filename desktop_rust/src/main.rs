4116b325e1a2611a635471b3db22ffd20a91b045
Solomon Sanderson <solomonsanderson@outlook.com>
2023-08-25 10:46:34 +0100

    removed cad file

--- desktop_rust/src/main.rs
+++ desktop_rust/src/main.rs
@@ -20,18 +20,22 @@ fn macro_finder() {
         test_port.set_timeout(timeout).expect("write failed");
         test_port.write(command.as_bytes()).expect("write failed");
 
-        for i in 0..3 {
+        for i in 0..10 {
             println!("{:?}",i);
-            let mut serial_buf: Vec<u8> = vec![0; 8];
-            let result = test_port.read(serial_buf.as_mut_slice());
-            let result_str = str::from_utf8(&serial_buf).unwrap();
-            println!("{:?}", result_str.trim());
+            // let mut serial_buf: Vec<u32> = vec![0; 32];
+            // let result = test_port.read_to_string(serial_buf.as_mut_slice());
+            // let result_str = str::from_utf8(&serial_buf).unwrap();
+
+            let mut buffer = String::new();
+
+            let result_str = test_port.read_to_string(&mut buffer);
+            println!("{:?}", result_str.to_string().as_str().trim());
             // let result_str = "serial";
-            if result_str.contains(command){
-                let connect_status = true;
-            } else {
-                let connect_status = false;
-            }
+            // if result_str.contains(command){
+            //     let connect_status = true;
+            // } else {
+            //     let connect_status = false;
+            // }
             
         }
         
@@ -42,26 +46,26 @@ fn macro_finder() {
 
 
 
-fn media_ctrl() {
-    #[cfg(not(target_os = "windows"))]
-    let hwnd = None;
-    #[cfg(target_os = "windows")]
-    let hwnd = {
-        use raw_window_handle::Win32WindowHandle;
-        let handle: Win32WindowHandle = unimplemented!();
-        Some(handle.hwnd)
-    };
-    let config = PlatformConfig {
-        dbus_name: "my_player",
-        display_name: "My Player",
-        hwnd,
-    };
+// fn media_ctrl() {
+//     #[cfg(not(target_os = "windows"))]
+//     let hwnd = None;
+//     #[cfg(target_os = "windows")]
+//     let hwnd = {
+//         use raw_window_handle::Win32WindowHandle;
+//         let handle: Win32WindowHandle = unimplemented!();
+//         Some(handle.hwnd)
+//     };
+//     let config = PlatformConfig {
+//         dbus_name: "my_player",
+//         display_name: "My Player",
+//         hwnd,
+//     };
 
-    let mut controls = MediaControls::new(config).unwrap();
-    controls.set_playback(souvlaki::MediaPlayback::Playing{progress: None});
-}
+//     let mut controls = MediaControls::new(config).unwrap();
+//     controls.set_playback(souvlaki::MediaPlayback::Playing{progress: None});
+// }
 
 fn main() {
-    media_ctrl();
+    // media_ctrl();
     macro_finder();
 }
